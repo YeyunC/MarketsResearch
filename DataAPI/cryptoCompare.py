16261f28fe6e4669e833ed2639259c1c2d621577
YeyunChen <yeyunc@ripple.com>
2024-01-04 15:51:24 +0000

    new cryptocompare api function enhance

--- DataAPI/cryptoCompare.py
+++ DataAPI/cryptoCompare.py
@@ -9,7 +9,7 @@ class cryptoCompareApi():
     def __init__(self):
         self.api_key = "bd7e829531032b62e5042190f9508096366df339e576b8469469ea1f4ddcd6d9"
 
-    def make_request(self, url, params):
+    def request_to_df(self, url, params={}):
         response = requests.get(url, params=params)
 
         if response.status_code == 200:
@@ -19,22 +19,56 @@ class cryptoCompareApi():
         else:
             return pd.DataFrame()
 
-    def load_date_range_data(self, start_date, end_date, freq, load_func: Callable, market, instrument):
-        to_ts = end_date.timestamp()
-        if freq == 'days':
-            limit = (end_date - start_date).days
-        elif freq == 'hours':
-            limit = (end_date - start_date).days * 24
-        elif freq == 'minutes':
-            limit = (end_date - start_date).days * 24 * 60
+    def request_to_dict(self, url, params={}):
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            data = response.json()["Data"]
+            return data
         else:
-            limit = 0
-        return load_func(freq, market, instrument, to_ts, limit)
+            return {}
+
+    def calc_n_period(self, start_date, end_date, freq):
+        limit = (end_date - start_date).total_seconds()
+        if freq == 'hours':
+            limit = limit / 60 / 60
+        elif freq == 'minutes':
+            limit = limit / 60
+        elif freq == 'days':
+            limit = limit / 60 / 60 / 24
+        return limit
+
+    def offset_period(self, time_ref, n, freq):
+        if freq == 'hours':
+            return time_ref - dt.timedelta(hours=n)
+        elif freq == 'minutes':
+            return time_ref - dt.timedelta(minutes=n)
+        else:
+            return time_ref - dt.timedelta(days=n)
+
+    def load_date_range_data(self, start_date, end_date, freq, load_func: Callable, market, instrument):
+
+        max_limit = 2000
+        data_list = []
+        new_end_date = end_date
+        n_period = self.calc_n_period(start_date, new_end_date, freq)
+
+        while n_period > max_limit:
+            tmp_data = load_func(freq, market, instrument, new_end_date.timestamp() - 1, max_limit)
+            data_list.append(tmp_data)
+
+            new_end_date = self.offset_period(new_end_date, max_limit, freq)
+            n_period = self.calc_n_period(start_date, new_end_date, freq)
+
+        tmp_data = load_func(freq, market, instrument, new_end_date.timestamp() - 1, n_period)
+        data_list.append(tmp_data)
+        return pd.concat(reversed(data_list))
 
     def load_funding_rate_historical_ohlcv(self, freq, market, instrument, to_ts, limit):
         if not freq in ['days', 'hours', 'minutes']:
             return
 
+        print(f'futures/v1/historical/funding-rate {market} {instrument} {freq} {int(to_ts)} {limit}')
         url = f'https://data-api.cryptocompare.com/futures/v1/historical/funding-rate/{freq}?'
         params = {
             'market': market,
@@ -42,24 +76,40 @@ class cryptoCompareApi():
             'to_ts': to_ts,
             'limit': limit
         }
-        data = self.make_request(url, params)
+        data = self.request_to_df(url, params)
         return data
 
     def proc_funding_rate_historical_ohlcv(self, data):
         data.index = [dt.datetime.fromtimestamp(x) for x in data['TIMESTAMP']]
         return data[['MARKET', 'INSTRUMENT', 'MAPPED_INSTRUMENT', 'CLOSE']]
 
+    def load_all_futures_markets_instruments(self):
+        url = 'https://data-api.cryptocompare.com/futures/v1/markets/instruments'
+        data = self.request_to_dict(url, {})
+        return data
+
+    def instrument_search(self, token):
+        meta_data = self.load_all_futures_markets_instruments()
+        for market, market_meta in meta_data.items():
+            for instru, instru_meta in market_meta['instruments'].items():
+                if token.lower() in str(instru).lower():
+                    print(f'{market} {instru}')
+
 
 if __name__ == '__main__':
     dataAPI = cryptoCompareApi()
-    # data = dataAPI.load_funding_rate_historical_ohlcv('hours', 'bitmex', 'XBTUSD', '1702857416', 1000)
 
-    data = dataAPI.load_date_range_data(
-        start_date=dt.datetime(2023, 1, 1),
-        end_date=dt.datetime(2024, 1, 1),
-        freq = 'hours',
-        load_func=dataAPI.load_funding_rate_historical_ohlcv,
-        market='bitmex',
-        instrument='XBTUSD')
-    data = dataAPI.proc_funding_rate_historical_ohlcv(data)
+    # data = dataAPI.load_date_range_data(
+    #     start_date=dt.datetime(2023, 1, 1),
+    #     end_date=dt.datetime(2024, 1, 1),
+    #     freq='hours',
+    #     load_func=dataAPI.load_funding_rate_historical_ohlcv,
+    #     market='bitmex',
+    #     instrument='XBTUSD')
+    # data = dataAPI.proc_funding_rate_historical_ohlcv(data)
+
+    data = dataAPI.load_all_futures_markets_instruments()
+    dataAPI.instrument_search('btc')
+
+    # data = dataAPI.load_all_futures_instruments(market='binance')
     print(data)
