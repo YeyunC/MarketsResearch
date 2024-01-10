d279493822c4c41f0bb2278bdbbbf232b45f5a07
YeyunChen <yeyunc@ripple.com>
2024-01-10 17:28:29 +0000

    Consolidate some code

--- DataAPI/cryptoCompare.py
+++ DataAPI/cryptoCompare.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import os
 from typing import Callable
 
 import pandas as pd
@@ -50,7 +51,7 @@ class cryptoCompareApi():
         else:
             return time_ref - dt.timedelta(days=n)
 
-    def load_date_range_data(self, start_date, end_date, freq, load_func: Callable, market, instrument):
+    def load_date_range_data(self, start_date, end_date, freq, load_func: Callable, mode, market, instrument):
 
         max_limit = 2000
         data_list = []
@@ -58,22 +59,32 @@ class cryptoCompareApi():
         n_period = self.calc_n_period(start_date, new_end_date, freq)
 
         while n_period > max_limit:
-            tmp_data = load_func(freq, market, instrument, new_end_date.timestamp() - 1, max_limit)
+            tmp_data = load_func(mode, freq, market, instrument, new_end_date.timestamp() - 1, max_limit)
             data_list.append(tmp_data)
 
             new_end_date = self.offset_period(new_end_date, max_limit, freq)
             n_period = self.calc_n_period(start_date, new_end_date, freq)
 
-        tmp_data = load_func(freq, market, instrument, new_end_date.timestamp() - 1, n_period)
+        tmp_data = load_func(mode, freq, market, instrument, new_end_date.timestamp() - 1, n_period)
         data_list.append(tmp_data)
         return pd.concat(reversed(data_list))
 
-    def load_funding_rate_historical_ohlcv(self, freq, market, instrument, to_ts, limit):
+    def load_historical_ohlcv(self, mode, freq, market, instrument, to_ts, limit):
         if not freq in ['days', 'hours', 'minutes']:
             return
 
-        print(f'futures/v1/historical/funding-rate {market} {instrument} {freq} {int(to_ts)} {limit}')
-        url = f'https://data-api.cryptocompare.com/futures/v1/historical/funding-rate/{freq}?'
+        if not mode in ['spot', 'futures', 'fundingrate']:
+            return
+
+        if mode == 'futures':
+            end_point = 'futures/v1/historical'
+        elif mode == 'fundingrate':
+            end_point = 'futures/v1/historical/funding-rate'
+        else:
+            end_point = 'spot/v1/historical'
+
+        print(f'{end_point} {market} {instrument} {freq} {int(to_ts)} {limit}')
+        url = f'https://data-api.cryptocompare.com/{end_point}/{freq}?'
         params = {
             'market': market,
             'instrument': instrument,
@@ -83,9 +94,13 @@ class cryptoCompareApi():
         data = self.request_to_df(url, params)
         return data
 
-    def proc_funding_rate_historical_ohlcv(self, data):
-        data.index = [dt.datetime.fromtimestamp(x) for x in data['TIMESTAMP']]
-        return data[['MARKET', 'INSTRUMENT', 'MAPPED_INSTRUMENT', 'CLOSE']]
+    def proc_historical_ohlcv(self, data):
+        data['TIMESTAMP'] = [dt.datetime.fromtimestamp(x) for x in data['TIMESTAMP']]
+        return data[[
+            'TIMESTAMP', 'CLOSE', 'TOTAL_TRADES', 'VOLUME',
+            'TOTAL_TRADES_BUY', 'TOTAL_TRADES_SELL',
+            'VOLUME_BUY', 'VOLUME_SELL'
+        ]]
 
     def load_all_meta_data(self, mode='spot'):
         if not mode in ['futures', 'spot', 'index']:
@@ -117,6 +132,32 @@ class cryptoCompareApi():
                 market_instru_list.append((market, instru))
         return market_instru_list
 
+    def load_annual_hourly_ohlc_data(self, mode, market, instrument):
+        project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        path_to_cache = os.path.join(project_root, 'Data', 'ohlc_annual_hourly')
+
+        for x in [mode, instrument, market]:
+            path_to_cache = os.path.join(path_to_cache, x)
+            if not os.path.exists(path_to_cache):
+                os.mkdir(path_to_cache)
+
+        file_path = os.path.join(path_to_cache, '2014.csv')
+        if not os.path.exists(file_path):
+            data = self.load_date_range_data(
+                start_date=dt.datetime(2023, 1, 1),
+                end_date=dt.datetime(2024, 1, 1),
+                freq='hours',
+                load_func=self.load_historical_ohlcv,
+                mode=mode,
+                market=market,
+                instrument=instrument)
+            data = self.proc_historical_ohlcv(data)
+            data.to_csv(file_path, index_label=False)
+
+        data = pd.read_csv(file_path)
+        data['TIMESTAMP'] = pd.to_datetime(data['TIMESTAMP'])
+        return data
+
 
 if __name__ == '__main__':
     dataAPI = cryptoCompareApi()
@@ -130,6 +171,9 @@ if __name__ == '__main__':
     #     instrument='XBTUSD')
     # data = dataAPI.proc_funding_rate_historical_ohlcv(data)
 
-
-    data = dataAPI.load_all_spots_markets_instruments()
-    print(data)
+    data = dataAPI.load_annual_hourly_ohlc_data(
+        mode='spot',
+        instrument='BTC-USDT',
+        market='bitstamp'
+    )
+    print(len(data))
