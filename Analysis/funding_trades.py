5d8e7dda6fb829ae58d9b2f13ebc056f2fcff3e1
YeyunChen <yeyunc@ripple.com>
2024-01-23 10:03:01 +0000

    push to the latest

--- Analysis/funding_trades.py
+++ Analysis/funding_trades.py
@@ -1,4 +1,3 @@
-import datetime as dt
 import os
 
 import matplotlib.pyplot as plt
@@ -47,8 +46,8 @@ def get_all_future_basis(instrument, market):
 
     spot_market = market
     if not market in spot_markets:
-        print(f'spot market {market} does not exist, using binance instead')
-        spot_market = 'binance'
+        spot_market = 'coinbase'
+        print(f'spot market {market} does not exist, using {spot_market} instead')
 
     try:
         df_spot = __dataAPI.load_annual_hourly_ohlc_data(
@@ -61,7 +60,7 @@ def get_all_future_basis(instrument, market):
     data_list = [df_spot]
 
     if market == 'CME':
-        data = cme.load_file()
+        data = cme.load_file(token=instrument.split('-')[0].lower())
         data_list.append(data)
         df = pd.concat(data_list, axis=1).sort_index()
         tmp_name = data.columns[0]
@@ -74,14 +73,15 @@ def get_all_future_basis(instrument, market):
     all_futures = __dataAPI.load_futures_instruments(underlying=instrument, market=market, style='VANILLA')
     for idx, fut in all_futures.iterrows():
         try:
-            data = __dataAPI.load_annual_hourly_ohlc_data(
-                mode='futures', instrument=fut['instrument'], market=fut['market'], start_year=2023)
+            if 'PERP' in fut['instrument']:
+                data = __dataAPI.load_annual_hourly_ohlc_data(
+                    mode='futures', instrument=fut['instrument'], market=fut['market'], start_year=2023)
 
-            if len(data) > 0:
-                tmp_name = get_fut_col_name(fut)
-                data = data[['TIMESTAMP', 'CLOSE']].rename({'CLOSE': tmp_name}, axis=1)
-                data = data.drop_duplicates(subset='TIMESTAMP', keep='last').set_index('TIMESTAMP')
-                data_list.append(data)
+                if len(data) > 0:
+                    tmp_name = get_fut_col_name(fut)
+                    data = data[['TIMESTAMP', 'CLOSE']].rename({'CLOSE': tmp_name}, axis=1)
+                    data = data.drop_duplicates(subset='TIMESTAMP', keep='last').set_index('TIMESTAMP')
+                    data_list.append(data)
         except:
             print(fut)
 
@@ -90,12 +90,11 @@ def get_all_future_basis(instrument, market):
         try:
             tmp_name = get_fut_col_name(fut)
             if tmp_name in df.columns:
-                df[tmp_name] = df[tmp_name] / df['spot'] - 1
+                # df[tmp_name] = df[tmp_name] / df['spot'] - 1
+                # df['basis'] = df[tmp_name] / df['spot'] - 1
 
                 tenor = fut['tenor']
                 if tenor == 'PERPETUAL':
-                    df[tmp_name] = df[tmp_name]
-
                     funding_rate = __dataAPI.load_annual_hourly_ohlc_data(
                         mode='fundingrate', instrument=fut['instrument'], market=fut['market'], start_year=2023)
                     funding_rate['CLOSE'] = funding_rate['CLOSE']
@@ -103,14 +102,25 @@ def get_all_future_basis(instrument, market):
                     funding_rate = funding_rate.drop_duplicates(subset='TIMESTAMP', keep='last').set_index('TIMESTAMP')
 
                     df = pd.concat([df, funding_rate], axis=1)
-                    df[tmp_name] = df[tmp_name] - df['FUNDING']
-                    df[tmp_name] = np.power((1 + (df[tmp_name] - df['FUNDING']) * 3), 365) - 1
+
+                    # if basis
+                    df[tmp_name] = df['spot'] / df[tmp_name] - 1
+                    df[tmp_name] = np.power((1 + df[tmp_name] * 3), 365) - 1
+
+                    # if funding
+                    # df[tmp_name] = np.power((1 + df['FUNDING'] * 3), 365) - 1
+
+                    # df[tmp_name] = np.power((1 + (df[tmp_name] - df['FUNDING']) * 3), 365) - 1
+
+                    # df['basis_annulized'] = np.power((1 + df['basis'] * 3), 365) - 1
+                    # df['funding_annulized'] = np.power((1 + df['FUNDING'] * 3), 365) - 1
+                    # df.to_csv('tmp.csv')
                     df.drop('FUNDING', axis=1, inplace=True)
-                else:
-                    expiry = dt.datetime.strptime(tenor, '%Y%m%d')
-                    today = dt.datetime.now()
-                    annulized_factor = 365 / (expiry - today).days
-                    df[tmp_name] = np.power(1 + df[tmp_name], annulized_factor) - 1
+                # else:
+                #     expiry = dt.datetime.strptime(tenor, '%Y%m%d')
+                #     today = dt.datetime.now()
+                #     annulized_factor = 365 / (expiry - today).days
+                #     df[tmp_name] = np.power(1 + df[tmp_name], annulized_factor) - 1
         except:
             print(fut)
 
@@ -119,7 +129,7 @@ def get_all_future_basis(instrument, market):
 
 
 def calc_stats(df):
-    quantiles = df.quantile([0.01, 0.05, 0.25, 0.5, 0.75, 0.95, 0.99])
+    quantiles = df.quantile([0.05, 0.95])
     quantiles.index = [f'percentile_{x:0.0%}' for x in quantiles.index]
 
     stats = pd.DataFrame([df.count(), df.mean(), df.std()], index=['n_datapoints', 'mean', 'std_dev'])
@@ -134,7 +144,7 @@ def calc_all_stats(instrument):
     # data_list = [df_funding]
     data_list = []
     all_future_markets = get_all_futures(instrument=instrument)['market'].unique()
-    if instrument == 'BTC-USDT':
+    if instrument in ('BTC-USD', 'ETH-USD'):
         all_future_markets = ['CME'] + list(all_future_markets)
 
     for mkt in all_future_markets:
@@ -146,18 +156,21 @@ def calc_all_stats(instrument):
     rlt = calc_stats(df)
     rlt = rlt.T
     for col in df.columns:
-        plot_column(df, col)
+        plot_column(df, col, instrument)
     return rlt
 
 
-def plot_column(df, col):
+def plot_column(df, col, instrument):
     plt.figure(figsize=(30, 10))
+    plt.axhline(y=0, color='lightgray', linestyle='-')
     plt.plot(df.index, df[col])
     plt.title(col)
     if 'PERP' in col:
         # lb = df[col].quantile(0.01)
-        # ub = df[col].quantile(0.99)
-        lb, ub = (-1, 5)
+        ub = df[col].quantile(0.99)
+        mb = df[col].max()
+        lb = -1
+        ub = max(min(mb, 5), ub)
         plt.ylim(lb, ub)
     # plt.legend()
     plt.tight_layout()
@@ -165,19 +178,21 @@ def plot_column(df, col):
     # write image to bytes
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     path_to_fig = os.path.join(project_root, 'Data', 'funding_basis_stats')
-    file_path = os.path.join(path_to_fig, f'fig_{col}.png')
+    file_path = os.path.join(path_to_fig, f'fig_{instrument}_{col}.png')
 
     plt.savefig(file_path, format='PNG')
 
 
 if __name__ == '__main__':
-    instrument = 'BTC-USDT'
+    instrument = 'ETH-USDT'
 
-    spot_market = get_all_spots(instrument='BTC-USDT')
-
+    spot_market = get_all_spots(instrument=instrument)
+    print(spot_market)
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     path_to_cache = os.path.join(project_root, 'Data', 'funding_basis_stats')
     file_path = os.path.join(path_to_cache, f'{instrument}_1Y.csv')
 
-    data = calc_all_stats(instrument='BTC-USDT')
+    data = calc_all_stats(instrument=instrument)
     data.to_csv(file_path)
+
+    # get_all_future_basis(instrument, market='binance')
--- DataAPI/cme.py
+++ DataAPI/cme.py
@@ -6,9 +6,10 @@ import pandas as pd
 import DataAPI.util as dutil
 
 
-def load_file(file_name='cme_btc_future_continous_2023.csv'):
+def load_file(token='btc'):
+    fn = f'cme_{token}_future_continous_2023.csv'
     root_dir = dutil.get_project_root()
-    file_path = os.path.join(root_dir, 'Data', 'cme_ohlc', file_name)
+    file_path = os.path.join(root_dir, 'Data', 'cme_ohlc', fn)
     data = pd.read_csv(file_path)
     data['date_time'] = data['Local Date'] + ' ' + data['Local Time']
     data['TIMESTAMP'] = [dt.datetime.strptime(x, "%d-%b-%Y %H:%M") for x in data['date_time']]
