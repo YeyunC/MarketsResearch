a967af632af9f5985902b63176bd4dc3f664e3a0
YeyunChen <yeyunc@ripple.com>
2024-01-23 16:55:36 +0000

    push to the latest

--- Analysis/funding_trades.py
+++ Analysis/funding_trades.py
@@ -1,7 +1,7 @@
+import datetime as dt
 import os
 
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
 from DataAPI import cme
@@ -40,7 +40,10 @@ def get_all_funding_rates(instrument):
 
 def get_all_future_basis(instrument, market):
     def get_fut_col_name(fut):
-        return f"basis_{fut['market']}|{fut['tenor']}"
+        return f"basis_{fut['tenor']}|{fut['market']}"
+
+    def get_funding_col_name():
+        return f"funding_{fut['tenor']}|{fut['market']}"
 
     spot_markets = list(get_all_spots(instrument)['market'])
 
@@ -65,23 +68,31 @@ def get_all_future_basis(instrument, market):
         df = pd.concat(data_list, axis=1).sort_index()
         tmp_name = data.columns[0]
         df[tmp_name] = df[tmp_name] / df['spot']
-        df[tmp_name] = np.power(df[tmp_name], 12) - 1
-
         df.drop('spot', axis=1, inplace=True)
         return df
 
     all_futures = __dataAPI.load_futures_instruments(underlying=instrument, market=market, style='VANILLA')
     for idx, fut in all_futures.iterrows():
         try:
-            if 'PERP' in fut['instrument']:
-                data = __dataAPI.load_annual_hourly_ohlc_data(
-                    mode='futures', instrument=fut['instrument'], market=fut['market'], start_year=2023)
 
-                if len(data) > 0:
-                    tmp_name = get_fut_col_name(fut)
-                    data = data[['TIMESTAMP', 'CLOSE']].rename({'CLOSE': tmp_name}, axis=1)
-                    data = data.drop_duplicates(subset='TIMESTAMP', keep='last').set_index('TIMESTAMP')
-                    data_list.append(data)
+            data = __dataAPI.load_annual_hourly_ohlc_data(
+                mode='futures', instrument=fut['instrument'], market=fut['market'], start_year=2023)
+
+            if len(data) > 0:
+                tmp_name = get_fut_col_name(fut)
+                data = data[['TIMESTAMP', 'CLOSE']].rename({'CLOSE': tmp_name}, axis=1)
+                data = data.drop_duplicates(subset='TIMESTAMP', keep='last').set_index('TIMESTAMP')
+                data_list.append(data)
+
+            if 'PERP' in fut['instrument']:
+                funding_rate = __dataAPI.load_annual_hourly_ohlc_data(
+                    mode='fundingrate', instrument=fut['instrument'], market=fut['market'], start_year=2023)
+                if len(funding_rate) > 0:
+                    tmp_name = get_funding_col_name()
+                    funding_rate = funding_rate[['TIMESTAMP', 'CLOSE']].rename({'CLOSE': tmp_name}, axis=1)
+                    funding_rate = funding_rate.drop_duplicates(subset='TIMESTAMP', keep='last').set_index('TIMESTAMP')
+                    data_list.append(funding_rate)
+
         except:
             print(fut)
 
@@ -90,32 +101,30 @@ def get_all_future_basis(instrument, market):
         try:
             tmp_name = get_fut_col_name(fut)
             if tmp_name in df.columns:
+                if 'basis_' in tmp_name:
+                    df[tmp_name] = df[tmp_name] / df['spot'] - 1
+
                 # df[tmp_name] = df[tmp_name] / df['spot'] - 1
                 # df['basis'] = df[tmp_name] / df['spot'] - 1
 
-                tenor = fut['tenor']
-                if tenor == 'PERPETUAL':
-                    funding_rate = __dataAPI.load_annual_hourly_ohlc_data(
-                        mode='fundingrate', instrument=fut['instrument'], market=fut['market'], start_year=2023)
-                    funding_rate['CLOSE'] = funding_rate['CLOSE']
-                    funding_rate = funding_rate[['TIMESTAMP', 'CLOSE']].rename({'CLOSE': 'FUNDING'}, axis=1)
-                    funding_rate = funding_rate.drop_duplicates(subset='TIMESTAMP', keep='last').set_index('TIMESTAMP')
+                # tenor = fut['tenor']
+                # if tenor == 'PERPETUAL':
+                #
+                #     df = pd.concat([df, funding_rate], axis=1)
+                #
+                #     # if basis
+                #
+                #     df[tmp_name] = np.power((1 + df[tmp_name] * 3), 365) - 1
 
-                    df = pd.concat([df, funding_rate], axis=1)
+                # if funding
+                # df[tmp_name] = np.power((1 + df['FUNDING'] * 3), 365) - 1
 
-                    # if basis
-                    df[tmp_name] = df['spot'] / df[tmp_name] - 1
-                    df[tmp_name] = np.power((1 + df[tmp_name] * 3), 365) - 1
+                # df[tmp_name] = np.power((1 + (df[tmp_name] - df['FUNDING']) * 3), 365) - 1
 
-                    # if funding
-                    # df[tmp_name] = np.power((1 + df['FUNDING'] * 3), 365) - 1
-
-                    # df[tmp_name] = np.power((1 + (df[tmp_name] - df['FUNDING']) * 3), 365) - 1
-
-                    # df['basis_annulized'] = np.power((1 + df['basis'] * 3), 365) - 1
-                    # df['funding_annulized'] = np.power((1 + df['FUNDING'] * 3), 365) - 1
-                    # df.to_csv('tmp.csv')
-                    df.drop('FUNDING', axis=1, inplace=True)
+                # df['basis_annulized'] = np.power((1 + df['basis'] * 3), 365) - 1
+                # df['funding_annulized'] = np.power((1 + df['FUNDING'] * 3), 365) - 1
+                # df.to_csv('tmp.csv')
+                # df.drop('FUNDING', axis=1, inplace=True)
                 # else:
                 #     expiry = dt.datetime.strptime(tenor, '%Y%m%d')
                 #     today = dt.datetime.now()
@@ -132,10 +141,28 @@ def calc_stats(df):
     quantiles = df.quantile([0.05, 0.95])
     quantiles.index = [f'percentile_{x:0.0%}' for x in quantiles.index]
 
-    stats = pd.DataFrame([df.count(), df.mean(), df.std()], index=['n_datapoints', 'mean', 'std_dev'])
+    # stats = pd.DataFrame([df.count(), df.mean(), df.std()], index=['n_datapoints', 'mean', 'std_dev'])
+    stats = pd.DataFrame([df.mean(), df.std()], index=['mean', 'std_dev'])
     df = pd.concat([stats, quantiles])
 
     return df
+
+
+def calc_stats_to_df(df, lookback):
+    filter_day = df.index.max() - dt.timedelta(days=lookback)
+    df_calc = df[df.index >= filter_day].copy()
+    rlt = calc_stats(df_calc)
+    rlt = annualize_df(rlt)
+    rlt = rlt.T.sort_index()
+    rlt['lookback'] = lookback
+    return rlt
+
+
+def plot_charts_look_back(df, lookback):
+    filter_day = df.index.max() - dt.timedelta(days=lookback)
+    df_calc = df[df.index >= filter_day].copy()
+    for col in df.columns:
+        plot_column(df_calc, col, instrument, lookback)
 
 
 def calc_all_stats(instrument):
@@ -143,7 +170,7 @@ def calc_all_stats(instrument):
     #
     # data_list = [df_funding]
     data_list = []
-    all_future_markets = get_all_futures(instrument=instrument)['market'].unique()
+    all_future_markets = get_all_futures(instrument=instrument).sort_values('market')['market'].unique()
     if instrument in ('BTC-USD', 'ETH-USD'):
         all_future_markets = ['CME'] + list(all_future_markets)
 
@@ -153,24 +180,49 @@ def calc_all_stats(instrument):
 
     df = pd.concat(data_list, axis=1)
 
-    rlt = calc_stats(df)
-    rlt = rlt.T
-    for col in df.columns:
-        plot_column(df, col, instrument)
+    rlt_list = []
+    for x in [90, 180, 365]:
+        rlt = calc_stats_to_df(df, lookback=x)
+        rlt_list.append(rlt)
+    df_rlt = pd.concat(rlt_list)
+
+    df = annualize_df(df)
+    for x in [90, 180, 365]:
+        plot_charts_look_back(df, x)
+    return df_rlt
+
+
+def annualize_df(rlt):
+    for col in rlt.columns:
+        if 'funding_' in col:
+            rlt[col] = rlt[col] * 3 * 365
+        elif 'basis_' in col:
+            if 'PERPETUAL' in col:
+                rlt[col] = rlt[col] * 3 * 365
+            elif 'cme' in col:
+                rlt[col] = rlt[col] * 12
+            else:
+                tenor = col.split('_')[-1].split('|')[0]
+                expiry = dt.datetime.strptime(tenor, '%Y%m%d')
+                today = dt.datetime.now()
+                annulized_factor = 365 / (expiry - today).days
+                rlt[col] = rlt[col] * annulized_factor
+
     return rlt
 
 
-def plot_column(df, col, instrument):
+def plot_column(df, col, instrument, lookback):
     plt.figure(figsize=(30, 10))
     plt.axhline(y=0, color='lightgray', linestyle='-')
     plt.plot(df.index, df[col])
     plt.title(col)
     if 'PERP' in col:
-        # lb = df[col].quantile(0.01)
+        lb = df[col].quantile(0.01)
         ub = df[col].quantile(0.99)
-        mb = df[col].max()
-        lb = -1
-        ub = max(min(mb, 5), ub)
+        maxb = df[col].max()
+        minb = df[col].min()
+        ub = max(min(maxb, 10), ub)
+        lb = min(max(minb, -10), lb)
         plt.ylim(lb, ub)
     # plt.legend()
     plt.tight_layout()
@@ -178,16 +230,17 @@ def plot_column(df, col, instrument):
     # write image to bytes
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     path_to_fig = os.path.join(project_root, 'Data', 'funding_basis_stats')
-    file_path = os.path.join(path_to_fig, f'fig_{instrument}_{col}.png')
+    file_path = os.path.join(path_to_fig, f'fig_{instrument}_{col}_{lookback}.png')
 
     plt.savefig(file_path, format='PNG')
 
 
 if __name__ == '__main__':
-    instrument = 'ETH-USDT'
+    instrument = 'BTC-USDT'
 
     spot_market = get_all_spots(instrument=instrument)
-    print(spot_market)
+    # print(spot_market)
+
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     path_to_cache = os.path.join(project_root, 'Data', 'funding_basis_stats')
     file_path = os.path.join(path_to_cache, f'{instrument}_1Y.csv')
